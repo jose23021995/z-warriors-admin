88c73252d79e0e924cb54154637f327e96bedffe
jose23021995 <80795569+jose23021995@users.noreply.github.com>
2026-04-25 13:21:50 -0600

    variables de texto de login

---
+++ spa/src/app/shared/interfaces/login-text.interface.ts
@@ -0,0 +1,12 @@
+export interface LoginText {
+    readonly title: string;
+    readonly subtitle: string;
+    readonly userLabel: string;
+    readonly passLabel: string;
+    readonly buttonText: string;
+}
+
+export interface DarkMode{
+    readonly DARK: string;
+    readonly LIGHT: string;
+}
---
+++ spa/src/app/shared/interfaces/text/login-texts.ts
@@ -0,0 +1,14 @@
+import { LoginText,DarkMode } from '../login-text.interface';
+
+export const LOGIN_TEXTS: LoginText = {
+    title: 'Z-Warriors Admin',
+    subtitle: 'Bienvenido al panel de control',
+    userLabel: 'Usuario',
+    passLabel: 'Contraseña',
+    buttonText: 'Acceder'
+};
+
+export const LOGIN_DARKMODE_TEXTS:DarkMode = {
+    DARK: "🌙 Modo Oscuro",
+    LIGHT: "☀️ Modo Claro",
+};
