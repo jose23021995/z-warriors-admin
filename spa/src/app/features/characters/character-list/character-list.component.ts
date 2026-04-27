a4d3da29ab445995f7600f9501e636d7c282e93f
jose23021995 <80795569+jose23021995@users.noreply.github.com>
2026-04-26 22:08:07 -0600

    declaracion de tabla

--- spa/src/app/features/characters/character-list/character-list.component.ts
+++ spa/src/app/features/characters/character-list/character-list.component.ts
@@ -1,8 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject, signal  } from '@angular/core';
 import { HeadersComponent } from '../../../shared/components/headers/headers'; // Importa la ruta correcta
+import { CardModule } from 'primeng/card';
+import {Table as TableComponent} from '../../../shared/components/table/table';
+
 @Component({
   selector: 'app-character-list',
-  imports: [HeadersComponent],
+  imports: [HeadersComponent,CardModule,TableComponent],
   templateUrl: './character-list.component.html',
   styleUrl: './character-list.component.scss',
 })
