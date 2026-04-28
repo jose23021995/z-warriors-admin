ecf2d3e6ee689c99410bc4d036dedfaec2754db2
jose23021995 <80795569+jose23021995@users.noreply.github.com>
2026-04-28 16:47:03 -0600

    convertir formulario en vista

--- spa/src/app/features/characters/components/character-detail/character-detail.ts
+++ spa/src/app/features/characters/components/character-detail/character-detail.ts
@@ -2,7 +2,6 @@ import { Component, OnInit, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
 import { DynamicDialogConfig, DynamicDialogRef } from 'primeng/dynamicdialog';
-
 // PrimeNG 18 Imports
 import { ButtonModule } from 'primeng/button';
 import { InputTextModule } from 'primeng/inputtext';
@@ -50,7 +49,6 @@ export class CharacterDetailComponent implements OnInit {
   public race!: string;
   public affiliation!: any;
   public deletedAt!: any;
-
   // Variables del Planeta
   public idPlanet!: number;
   public deletedAtPlanet!: null | string | Date;
@@ -58,18 +56,9 @@ export class CharacterDetailComponent implements OnInit {
   public imagePlanet!: string;
   public isDestroyed!: boolean;
   public namePlanet!: string;
-
   // Transformaciones
   public transformations: Transformation[] = [];
-
-  races = [
-    { label: 'Saiyan', value: 'Saiyan' },
-    { label: 'Namekian', value: 'Namekian' },
-    { label: 'Human', value: 'Human' }
-  ];
-
   ngOnInit() {
-    // Desestructuramos del objeto recibido (ModalCharacter)
     const { type, response:character,transformations:transformation } = this.config.data;
     this.characterResponse=character;
     console.log("informacion que llega del padre",this.config.data);
@@ -109,63 +98,10 @@ export class CharacterDetailComponent implements OnInit {
     this.isDestroyed = isDest;
     this.namePlanet = namePl;
     this.isEditable = type;
-    this.characterForm = this.fb.group({
-      name: [character.name, [Validators.required, Validators.maxLength(50)]],
-      ki: [character.ki, [Validators.required]],
-      maxKi: [character.maxKi, [Validators.required]],
-      race: [character.race, Validators.required],
-      gender: [character.gender, Validators.required],
-      // AUMENTAMOS EL LIMITE AQUÍ:
-      description: [character.description, [Validators.required, Validators.maxLength(1000)]], 
-      // O quita 'affiliation' si no vas a poner un input para ella:
-      affiliation: [character.affiliation] 
-    });
+    this.characterForm = this.fb.group({});
 
     if (!this.isEditable) this.characterForm.disable();
   }
-
-  onSave() {
-    alert('Su personaje fue editado');
-    this.ref.close(this.characterForm.value);
-  }
-
-  // En character-detail.ts
-
-// En character-detail.ts
-
-onFileSelect(event: any) {
-  // PrimeNG 18 suele usar event.currentFiles o event.files
-  const file = event.files ? event.files[0] : event.currentFiles[0];
-
-  if (file) {
-    const reader = new FileReader();
-    
-    reader.onload = (e: any) => {
-      // Esto actualizará [src]="image" de tu img.img-render automáticamente
-      this.image = e.target.result;
-      console.log("Imagen actualizada correctamente");
-    };
-
-    reader.readAsDataURL(file);
-  }
-}
-
-getKiPercentage(): number {
-  const kiValue = this.characterForm.get('ki')?.value;
-  const maxKiValue = this.characterForm.get('maxKi')?.value;
-
-  if (!kiValue || !maxKiValue) return 0;
-
-  // Limpiamos los puntos y convertimos a número
-  const ki = parseFloat(kiValue.toString().replace(/\./g, ''));
-  const maxKi = parseFloat(maxKiValue.toString().replace(/\./g, ''));
-
-  if (isNaN(ki) || isNaN(maxKi)) return 0;
-
-  const percentage = (ki / maxKi) * 100;
-  return percentage > 100 ? 100 : percentage;
-}
-
   onCancel() {
     this.ref.close();
   }
